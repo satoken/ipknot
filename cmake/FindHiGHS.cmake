3f7801f988f4b8467bbc8589ed1ee17fed0f2f0f
Kengo SATO <satoken@mail.dendai.ac.jp>
2023-02-28 09:01:08 +0000

    remove Japanese comments

--- cmake/FindHiGHS.cmake
+++ cmake/FindHiGHS.cmake
@@ -1,11 +1,7 @@
-# インクルードディレクトリのパスをgmp.hを頼りに検索する
 find_path(HiGHS_INCLUDE_DIR Highs.h
-  # 検索するパス
   PATHS
-    # 環境変数GMP_ROOTまたはGMP_INCLUDE_DIRが存在したらそこを検索
     ENV HiGHS_ROOT
     ENV HiGHS_INCLUDE_DIR
-    # CMakeの変数としてGMP_ROOTが定義されていたらそこを検索
     ${HiGHS_ROOT}
     /usr
     /usr/local
@@ -13,7 +9,7 @@ find_path(HiGHS_INCLUDE_DIR Highs.h
     include
     include/highs
   )
-# ライブラリへのパスをライブラリ名を元に検索する
+
 find_library(HiGHS_LIBRARY
   NAMES
     highs
@@ -26,7 +22,7 @@ find_library(HiGHS_LIBRARY
   PATH_SUFFIXES
     lib
   )
-# advancedモードでない限り変数の存在を隠す
+
 mark_as_advanced(HiGHS_INCLUDE_DIR HiGHS_LIBRARY)
 
 include(FindPackageHandleStandardArgs)
@@ -36,14 +32,9 @@ find_package_handle_standard_args(HiGHS
     HiGHS_LIBRARY
   )
 
-# GMPが見つかり、かつGMP::GMPが定義されていない場合
 if(HiGHS_FOUND AND NOT TARGET HiGHS::HiGHS)
-  # GMP::GMPというターゲット名でGMPライブラリを定義
-  # UNKNOWN = STATIC/SHAREDかはまだ不明
-  # IMPORTED = このプロジェクトに属さないターゲット
   add_library(HiGHS::HiGHS UNKNOWN IMPORTED)
   set_target_properties(HiGHS::HiGHS PROPERTIES
-    # C言語、C++なら"CXX"とする
     IMPORTED_LINK_INTERFACE_LANGUAGES "CXX"
     IMPORTED_LOCATION "${HiGHS_LIBRARY}"
     INTERFACE_INCLUDE_DIRECTORIES "${HiGHS_INCLUDE_DIR}"
